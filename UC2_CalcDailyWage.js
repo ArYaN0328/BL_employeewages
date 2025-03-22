61b40842d5e7b0ee9e3024da655decd7866c118c
ArYaN0328 <121503458+ArYaN0328@users.noreply.github.com>
2025-03-22 11:26:49 +0530

    functionforwage

--- UC2_CalcDailyWage.js
+++ UC2_CalcDailyWage.js
@@ -17,7 +17,6 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-
 let empHrs = 0;
 let empCheck = Math. floor (Math. random () * 10) & 3;
 empHrs = getWorkingHours (empCheck) ;
